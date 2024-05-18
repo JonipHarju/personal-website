87b83d883b8a17a8da33917dc034e3642efc0cd7
joni <jonipharju@icloud.com>
2024-05-19 01:11:57 +0300

    initialize next.js + tailwind + shadcn

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/components/Hero";
+
+export default function Home() {
+  return (
+    <main className="bg-black w-screen h-screen text-white">
+      <section className="bg-black w-screen h-screen flex items-center justify-center ">
+        <Hero />
+      </section>
+    </main>
+  );
+}
