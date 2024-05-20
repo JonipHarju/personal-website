eced865886100b63ec87b2950450113dce13330d
Joni Harju <jonipharju@gmail.com>
2024-05-21 00:26:39 +0300

    add background box element to hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
 import Hero from "@/components/Hero";
+import { BoxesCore } from "@/components/ui/background-boxes";
 
 export default function Home() {
-  return (
-    <main className="bg-black w-screen h-screen text-white">
-      <section className="bg-black w-screen h-screen flex items-center justify-center ">
-        <Hero />
-      </section>
-    </main>
+  return (  <>
+<div className="h-screen w-screen relative overflow-hidden bg-black flex flex-col items-center justify-center rounded-lg">
+  <div className="absolute inset-0 w-full h-full bg-black z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
+
+  <BoxesCore className="w-full h-full" />
+  <div className="text-center z-20">
+  <h1 className="text-white text-7xl">
+    Joni Harju
+  </h1>
+  <h2 className="mt-2 text-white text-6xl">
+    A god developer 
+  </h2>
+  </div>
+</div>
+</>
+
   );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,8 +3,9 @@ export default function Hero() {
   return (
     <div className="text-center">
       <h1 className="text-7xl">Moi</h1>
-      <h2 className="text-6xl">oon Joni</h2>
+      <h2 className="text-6xl text-red-600">oon Joni</h2>
       <h3 className="text-5xl">lol</h3>
     </div>
+    
   );
 }
